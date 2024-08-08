3e9a388b0361fa7a30ca066fd912522b9318662c
Andre <andressouza25@gmail.com>
2024-08-08 19:27:11 -0300

    feat: add React Hook Form validation to Login Page and Custom Input

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,5 +1,6 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
+import { useForm } from 'react-hook-form'
 
 // Components
 import CustomButton from '../../components/custom-button/custom-button.component'
@@ -13,9 +14,21 @@ import {
   LoginInputContainer,
   LoginSubtitle
 } from './login.styles'
-import CustomInput from '../../components/custom-input/custom-input.component.styles'
+import CustomInput from '../../components/custom-input/custom-input.component'
 
 const LoginPage = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm()
+
+  const handleSubmitPress = (data: any) => {
+    console.log(data)
+  }
+
+  console.log(errors)
+
   return (
     <>
       <Header />
@@ -31,15 +44,28 @@ const LoginPage = () => {
 
           <LoginInputContainer>
             <p>E-mail</p>
-            <CustomInput placeholder='Digite seu email' />
+            <CustomInput
+              hasError={!!errors?.email}
+              placeholder='Digite seu email'
+              {...register('email', { required: true })}
+            />
           </LoginInputContainer>
 
           <LoginInputContainer>
             <p>Senha</p>
-            <CustomInput placeholder='Digite sua senha' />
+            <CustomInput
+              hasError={!!errors?.password}
+              placeholder='Digite sua senha'
+              {...register('password', { required: true })}
+            />
           </LoginInputContainer>
 
-          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
+          <CustomButton
+            startIcon={<FiLogIn size={18} />}
+            onClick={() => handleSubmit(handleSubmitPress)()}
+          >
+            Entrar
+          </CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
