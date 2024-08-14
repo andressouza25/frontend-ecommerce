7896f2d2cc877d703ac2849dbf5d4daf5b226637
Andre <andressouza25@gmail.com>
2024-08-14 14:55:56 -0300

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBeUkz_FcJydXsgYe73l60D5cmlbQ6SPBI',
+  authDomain: 'club-ecommerce-e5d65.firebaseapp.com',
+  projectId: 'club-ecommerce-e5d65',
+  storageBucket: 'club-ecommerce-e5d65.appspot.com',
+  messagingSenderId: '361688005463',
+  appId: '1:361688005463:web:66eebba2d7edc4b18f9c12'
+}
+
+export const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
