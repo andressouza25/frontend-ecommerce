e0564b164dc0b09672dc6c9ae8bce3c729c337f1
Andre <andressouza25@gmail.com>
2024-08-14 15:36:20 -0300

    feat: get categories from firestore db

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { getDocs, collection } from 'firebase/firestore'
 
 // Components
 import CategoryItem from '../category-item/category-item.component'
@@ -9,7 +10,8 @@ import { CategoriesContainer, CategoriesContent } from './categories.styles'
 
 // Utilities
 import Category from '../../types/category.types'
-import env from '../../config/env.config'
+import { db } from '../../config/firebase.config'
+import { categoryConverter } from '../converters/firestore.converters'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
@@ -18,9 +20,17 @@ const Categories = () => {
 
   const fetchCategories = async () => {
     try {
-      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+      const categoriesFromFirestore: Category[] = []
 
-      setCategories(data)
+      const querySnapshot = await getDocs(
+        collection(db, 'categories').withConverter(categoryConverter)
+      )
+
+      querySnapshot.forEach((doc) => {
+        categoriesFromFirestore.push(doc.data())
+      })
+
+      setCategories(categoriesFromFirestore)
     } catch (error) {
       console.log({ error })
     }
---
+++ src/types/product.types.ts
@@ -0,0 +1,8 @@
+interface Product {
+  id: string
+  imageUrl: string
+  name: string
+  proce: number
+}
+
+export default Product
