9f48b65e6451818b73c9fff9304420344325ac31
Andre <andressouza25@gmail.com>
2024-08-16 14:30:53 -0300

    feat: add login

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,7 +1,7 @@
 import { BsGoogle } from 'react-icons/bs'
 import { FiLogIn } from 'react-icons/fi'
 import { useForm } from 'react-hook-form'
-import { isEmail } from 'validator'
+import validator from 'validator'
 
 // Components
 import CustomButton from '../../components/custom-button/custom-button.component'
@@ -17,6 +17,12 @@ import {
   LoginInputContainer,
   LoginSubtitle
 } from './login.styles'
+import {
+  AuthError,
+  AuthErrorCodes,
+  signInWithEmailAndPassword
+} from 'firebase/auth'
+import { auth } from '../../config/firebase.config'
 
 interface LoginForm {
   email: string
@@ -26,17 +32,37 @@ interface LoginForm {
 const LoginPage = () => {
   const {
     register,
-    formState: { errors },
-    handleSubmit
+    handleSubmit,
+    setError,
+    formState: { errors }
   } = useForm<LoginForm>()
 
-  const handleSubmitPress = (data: any) => {
-    console.log(data)
+  const handleSubmitPress = async (data: LoginForm) => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+
+      console.log({ userCredentials })
+    } catch (error) {
+      const _error = error as AuthError
+
+      if (_error.code === AuthErrorCodes.INVALID_PASSWORD) {
+        return setError('password', { type: 'mismatch' })
+      }
+
+      if (_error.code === AuthErrorCodes.USER_DELETED) {
+        return setError('email', { type: 'notFound' })
+      }
+    }
   }
 
   return (
     <>
       <Header />
+
       <LoginContainer>
         <LoginContent>
           <LoginHeadline>Entre com a sua conta</LoginHeadline>
@@ -45,22 +71,29 @@ const LoginPage = () => {
             Entrar com o Google
           </CustomButton>
 
-          <LoginSubtitle>ou entre com seu e-mail</LoginSubtitle>
+          <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
           <LoginInputContainer>
             <p>E-mail</p>
             <CustomInput
               hasError={!!errors?.email}
-              placeholder='Digite seu email'
+              placeholder='Digite seu e-mail'
               {...register('email', {
                 required: true,
                 validate: (value) => {
-                  return isEmail(value)
+                  return validator.isEmail(value)
                 }
               })}
             />
-            {errors?.email?.type === 'require' && (
-              <InputErrorMessage> O e-mail é obrigatório</InputErrorMessage>
+
+            {errors?.email?.type === 'required' && (
+              <InputErrorMessage>O e-mail é obrigatório.</InputErrorMessage>
+            )}
+
+            {errors?.email?.type === 'notFound' && (
+              <InputErrorMessage>
+                O e-mail não foi encontrado.
+              </InputErrorMessage>
             )}
 
             {errors?.email?.type === 'validate' && (
@@ -79,8 +112,12 @@ const LoginPage = () => {
               {...register('password', { required: true })}
             />
 
-            {errors?.password?.type === 'require' && (
-              <InputErrorMessage> A senha é obrigatória</InputErrorMessage>
+            {errors?.password?.type === 'required' && (
+              <InputErrorMessage>A senha é obrigatória.</InputErrorMessage>
+            )}
+
+            {errors?.password?.type === 'mismatch' && (
+              <InputErrorMessage>A senha é inválida.</InputErrorMessage>
             )}
           </LoginInputContainer>
 
