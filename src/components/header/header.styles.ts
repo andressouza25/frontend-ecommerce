5cda7d05bd6088692a0a7b5631cca29ebbd1288c
Andre <andressouza25@gmail.com>
2024-10-01 16:25:29 -0300

    style: add responsiveness to Header component

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -8,6 +8,12 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 export const HeaderTitle = styled.h2`
   font-weight: bold;
@@ -16,12 +22,20 @@ export const HeaderTitle = styled.h2`
   &:hover {
     cursor: pointer;
   }
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
+  }
 `
 
 export const HeaderItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
 `
 export const HeaderItem = styled.div`
   font-weight: 600;
@@ -33,6 +47,9 @@ export const HeaderItem = styled.div`
   &:nth-child(2),
   &:nth-child(3) {
     margin-right: 40px;
+    @media (max-width: 768px) {
+      margin-right: 0;
+    }
   }
 
   &:hover {
