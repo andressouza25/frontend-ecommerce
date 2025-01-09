c81ff6d998f95cb2b906413d6fa50088f283274d
Andre <andressouza25@gmail.com>
2025-01-09 15:21:30 -0300

    test: ensure categories fetches and shows categories

---
+++ src/components/categories/categories.test.tsx
@@ -0,0 +1,29 @@
+import * as firestore from 'firebase/firestore'
+import { renderWithRedux } from '../../helpers/test.helpers'
+import Categories from './categories.component'
+
+jest.mock('firebase/firestore')
+
+describe('Categories', () => {
+  it('should fetch and show categories', async () => {
+    const mockedFirestore = firestore as any
+    mockedFirestore.getDocs.mockReturnValue([
+      {
+        data() {
+          return {
+            id: '1',
+            displayName: 'Lorem Ipsum'
+          }
+        }
+      }
+    ])
+
+    mockedFirestore.collection.mockReturnValue({
+      withConverter: () => {}
+    })
+    const { getByText, findByText } = renderWithRedux(<Categories />, {})
+
+    await findByText(/lorem ipsum/i)
+    getByText(/explorar/i)
+  })
+})
